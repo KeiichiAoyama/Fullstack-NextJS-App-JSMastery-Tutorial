7c42e00364c4b9eb9dd47fdc4dbe7d17bb5580c4
ZinedineZethro <zethrozinedine@gmail.com>
2024-03-03 11:25:21 +0700

    figuring out onClick error

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,21 +3,23 @@ import Nav from '@components/Nav'
 import Provider from '@components/Provider'
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts"
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts"
 }
 
-function RootLayout({children}) {
+function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <body>
         <div className='main'>
-            <div className='gradient' />
+          <div className='gradient' />
         </div>
 
         <main className='app'>
-            <Nav />
-            {children}
+          <Nav />
+          {children}
         </main>
+      </body>
     </html>
   )
 }
